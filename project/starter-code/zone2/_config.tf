23a6b8cf77fdf6a91365ede7d2e600c37e40c265
CLOUDITIN DIGITAL <61991235+cloud-it-in@users.noreply.github.com>
2024-09-30 20:17:53 +0700

    Update _config.tf

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-dalvipk-west"
+     bucket = "udacity-tf-dalvipk-west1"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
