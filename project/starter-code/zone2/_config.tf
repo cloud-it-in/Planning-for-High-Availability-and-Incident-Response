a6b928268c0b2a46291f7280c6b9f612090b0f5b
PD-CLOUDITIN <pradip.k.dalvi@outlook.com>
2024-10-02 02:16:20 +0700

    ok

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -5,14 +5,6 @@ terraform {
      region = "us-west-1"
    }
  }
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "udacity-zone2-p2"
-    key    = "vpc/terraform.tfstate"
-    region = "us-west-1"
-  }
-}
 
  provider "aws" {
    region = "us-west-1"
