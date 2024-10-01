b83f71437c31f63cdd3bda618f2b49b87e300f38
PD-CLOUDITIN <pradip.k.dalvi@outlook.com>
2024-10-02 01:30:35 +0700

    ok

--- project/starter-code/zone2/modules/alb/alb.tf
+++ project/starter-code/zone2/modules/alb/alb.tf
@@ -3,12 +3,13 @@ resource "aws_lb_target_group" "udacity" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "instance"  # Added target_type for clarity
 }
 
 resource "aws_lb_target_group_attachment" "udacity" {
   count            = 2
   target_group_arn = aws_lb_target_group.udacity.arn
-  target_id        = var.ec2.*.id[count.index]
+  target_id        = element(var.ec2, count.index).id  # Updated to use element function
   port             = 80
 }
 
@@ -17,15 +18,14 @@ resource "aws_lb" "udacity" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [var.ec2_sg]
-  subnets            = var.subnet_id
+  subnets            = var.subnet_ids  # Corrected to plural
 
   enable_deletion_protection = false
-
 }
 
 resource "aws_lb_listener" "udacity" {
   load_balancer_arn = aws_lb.udacity.arn
-  port              = "80"
+  port              = 80  # Changed to integer
   protocol          = "HTTP"
 
   default_action {
