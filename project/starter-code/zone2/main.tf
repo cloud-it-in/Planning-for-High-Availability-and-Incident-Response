867863f20c98d397de1a1c9951e32e2a2d4dd4e4
PD-CLOUDITIN <pradip.k.dalvi@outlook.com>
2024-10-02 01:45:20 +0700

    ok

--- project/starter-code/zone2/main.tf
+++ project/starter-code/zone2/main.tf
@@ -28,7 +28,7 @@ module "vpc_west" {
   cidr_block = "10.100.0.0/16"
   account_owner = local.name
   name          = "${local.name}-project"
-  azs           = ["us-west-1b", "us-west-1c"]
+  azs           = ["us-west-1a", "us-west-1b"]
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
