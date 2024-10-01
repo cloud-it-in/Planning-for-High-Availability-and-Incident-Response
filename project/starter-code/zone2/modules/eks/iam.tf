a8b20b7d283d8bb2a4afb397c58f9f35a4a1fc26
PD-CLOUDITIN <pradip.k.dalvi@outlook.com>
2024-10-02 04:18:35 +0700

    ok

--- project/starter-code/zone2/modules/eks/iam.tf
+++ project/starter-code/zone2/modules/eks/iam.tf
@@ -11,19 +11,19 @@
    }
  }
 
-#   resource "aws_iam_role" "eks_cluster_role" {
-#    name               = "app-${var.name}-eks-cluster-role"
-#    assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
-#  }
+  resource "aws_iam_role" "eks_cluster_role" {
+   name               = "app-${var.name}-eks-cluster-role1"
+   assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+ }
 
   resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-   role       = "app-udacity-eks-cluster-role"
+   role       = aws_iam_role.eks_cluster_role.name
  }
 
   resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-   role       = "app-udacity-eks-cluster-role"
+   role       = aws_iam_role.eks_cluster_role.name
  }
 
   # --------- EKS NODE ---------------------
@@ -39,37 +39,37 @@
    }
  }
 
-#   resource "aws_iam_role" "eks_node_cluster_role" {
-#    name               = "app-${var.name}-eks-node-role"
-#    assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role_policy.json
-#  }
+  resource "aws_iam_role" "eks_node_cluster_role" {
+   name               = "app-${var.name}-eks-node-role1"
+   assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role_policy.json
+ }
 
   resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSCloudwatchPolicy" {
-   role       = "app-udacity-eks-node-role"
-   policy_arn = var.cloudwatch_policy.arn
+   role       = aws_iam_role.eks_node_cluster_role.name
+   policy_arn = aws_iam_policy.eks_cluster_role_cloudwatch_policy.arn
  }
 
   resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-   role       = "app-udacity-eks-node-role"
+   role       = aws_iam_role.eks_node_cluster_role.name
  }
 
   resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-   role       = "app-udacity-eks-node-role"
+   role       = aws_iam_role.eks_node_cluster_role.name
  }
 
   resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-   role       = "app-udacity-eks-node-role"
+   role       = aws_iam_role.eks_node_cluster_role.name
  }
 
   resource "aws_iam_role_policy_attachment" "node_CloudWatchAgentServerPolicy" {
    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-   role       = "app-udacity-eks-node-role"
+   role       = aws_iam_role.eks_node_cluster_role.name
  }
 
   resource "aws_iam_role_policy_attachment" "node_harmony_policy_attachment" {
-   policy_arn = var.instance_policy.arn
-   role       = "app-udacity-eks-node-role"
+   policy_arn = aws_iam_policy.eks_node_instance_policy.arn
+   role       = aws_iam_role.eks_node_cluster_role.name
  }
